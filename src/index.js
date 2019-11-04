305ab8536dfb3e002968879b1338cf2b00d784fc
Wenderson <wps713@gmail.com>
2019-11-04 14:11:01 -0300

    Simplifying index.js to the basics

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import React from 'react';
 
-import {StyleSheet, View, Text} from 'react-native';
+import {View} from 'react-native';
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff',
-  },
-});
-console.tron.log('Hello world');
-
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-    </View>
-  );
+  return <View />;
 }
