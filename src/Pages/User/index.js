403f79ddfd357d031d612265812d1488cc3617eb
Wenderson <wps713@gmail.com>
2019-11-05 21:56:16 -0300

    Adding api in users page, and getting starred repositories

--- src/Pages/User/index.js
+++ src/Pages/User/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
+import api from '../../services/api';
 
 export default class User extends Component {
   static navigationOptions = ({navigation}) => ({
@@ -9,6 +10,15 @@ export default class User extends Component {
   state = {
     stars: [],
   };
+
+  async componentDidMount() {
+    const {navigation} = this.props;
+    const user = navigation.getParams('user');
+
+    const response = await api.get(`users/${user.login}/starred`);
+
+    this.state({stars: response.data});
+  }
 
   render() {
     return <View />;
