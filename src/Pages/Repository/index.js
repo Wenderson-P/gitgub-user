d018de3a0f943e98e077d3e7f5d4951a75ce51d9
Wenderson <wps713@gmail.com>
2019-11-06 01:15:00 -0300

    Adding repository title

--- src/Pages/Repository/index.js
+++ src/Pages/Repository/index.js
@@ -1,7 +1,17 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import {WebView} from 'react-native-webview';
 
 export default function Repository({navigation}) {
   const repositoryUrl = navigation.getParam('repositoryUrl');
   return <WebView source={{uri: repositoryUrl}} />;
 }
+Repository.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+  }).isRequired,
+};
+
+Repository.navigationOptions = ({navigation}) => ({
+  title: navigation.getParam('repositoryName'),
+});
