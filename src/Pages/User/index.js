22d39d520cdb1d50c1800ac9d9726ca5df6ab232
Wenderson <wps713@gmail.com>
2019-11-05 21:53:25 -0300

    Transforming user page in a class component,Adding Header.
    The header is the user name passed through the params

--- src/Pages/User/index.js
+++ src/Pages/User/index.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View} from 'react-native';
 
-export default function User(props) {
-  console.tron.log(props);
-  return <View />;
+export default class User extends Component {
+  static navigationOptions = ({navigation}) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  state = {
+    stars: [],
+  };
+
+  render() {
+    return <View />;
+  }
 }
