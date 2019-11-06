59435d554dc2064da5a43e62cf4238f1b7708a92
Wenderson <wps713@gmail.com>
2019-11-05 22:08:23 -0300

    Adding code for user page

--- src/Pages/User/index.js
+++ src/Pages/User/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Proptypes from 'prop-types';
 import {View} from 'react-native';
 import api from '../../services/api';
+import {Container, Header, Avatar, Name, Bio} from './styles';
 
 export default class User extends Component {
   static navigationOptions = ({navigation}) => ({
@@ -22,7 +23,18 @@ export default class User extends Component {
   }
 
   render() {
-    return <View />;
+    const {navigation} = this.props;
+    const {stars} = this.props;
+    const user = navigation.getParam('user');
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{uri: user.avatar}} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+      </Container>
+    );
   }
 }
 
